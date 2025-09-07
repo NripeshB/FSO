e99b1aa994f5ba6b04486ac697efdb597abc26d4
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-09-07 14:34:56 +0530

    fso part2 exercise 2.8 solved

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,44 +2,54 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,number: '040-1234567' }
   ]) 
+  const [newNumber, setNewNumber] = useState('') 
   const [newName, setNewName] = useState('')
-const handleChange = (event)=>{
-  setNewName(event.target.value)
-}
-const handleOnClick = (event)=>{
-  event.preventDefault()
-  if(newName ===""){
-    alert("Enter a valid name")
+
+
+  const handleNameChange = (event)=>{
+    setNewName(event.target.value)
   }
-  else if(persons.some(person=>person.name === newName)){
-    alert(`${newName} is already added to the phonebook`)
+  const handleNumberChange = (event)=>{
+    setNewNumber(event.target.value)
   }
-  else{
-const newNameObj = {
-    name: newName
-  }
-  setPersons(persons.concat(newNameObj))
-  setNewName("")
-  }
+
+
+  const handleOnClick = (event)=>{
+    event.preventDefault()
+    if(newName ===""){
+      alert("Enter a valid name")
+    }
+    else if(persons.some(person=>person.name === newName)){
+      alert(`${newName} is already added to the phonebook`)
+    }
+    else{
+    const newNameObj = {
+      name: newName,
+      number: newNumber
+    }
+      setPersons(persons.concat(newNameObj))
+    setNewName("")
+    setNewNumber("")
+    }
   
-  
-}
+  }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleChange} value={newName} />
+          name: <input onChange={handleNameChange} value={newName} />
         </div>
+         <div>number: <input onChange = {handleNumberChange} value = {newNumber} /></div>
         <div>
           <button type="submit" onClick={handleOnClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => (
-        <div key={person.name}> {person.name}</div>
+        <div key={person.name}> {person.name} {person.number}</div>
       ))}
     </div>
   )
