70afc2d7d4a71428bff1014318247ead4d4040d8
Nripesh <nripesh.bhusal@gmail.com>
2025-09-27 16:54:04 +0530

    testing backend

--- part3/index.js
+++ part3/index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json()); 
+app.use(express.static('dist'))
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
@@ -84,7 +85,5 @@ app.delete('/api/persons/:id', (request, response)=>{
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log("Hey there! ")
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
