f0565dfabcc6ec29a5c80d8db4b297cb6115d08c
Nripesh <nripesh.bhusal@gmail.com>
2025-09-27 17:13:52 +0530

    testing_1

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express()
+const path = require('path')
 const cors = require('cors')
+const app = express()
 app.use(cors())
 app.use(express.json()); 
 app.use(express.static('dist'))
