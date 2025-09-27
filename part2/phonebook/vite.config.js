cb82ec78b4edf0c17b0c86e815b1d05571d07c0a
Nripesh <nripesh.bhusal@gmail.com>
2025-09-27 17:09:21 +0530

    .

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fso-phonebook-9ek5.onrender.com',
+        changeOrigin: true,
+      },
+    }
+  },
 })
