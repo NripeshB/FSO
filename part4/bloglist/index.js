5da5dd7d861120ee43c6c459e7922d200a55ca74
Nripesh <nripesh.bhusal@gmail.com>
2025-09-30 15:54:33 +0530

    fso part4 exercise 4.2 solved

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,36 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+const app = require('./app') 
 const config = require('./utils/config')
-const app = express()
-app.use(express.json()) 
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-mongoose.connect(config.MONGODB_URI)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
