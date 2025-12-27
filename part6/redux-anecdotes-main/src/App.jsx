e26630b8ef0197211fbf8d354bc48c51fad70bf3
Nripesh <nripesh.bhusal@gmail.com>
2025-12-27 20:56:25 +0530

    part6 exercise 6.20 to 6.24 solved

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -1,31 +1,75 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+// import { useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import AnecdoteList from './components/AnecdoteList'
+// import NewAnecdote from './components/NewAnecdotes'
+// import Notification from './components/Notification'
+// import Filter from './components/Filter'
+// import { initializeAnecdotes } from './reducers/anecdoteReducer'
+// const App = () => {
+//   const dispatch = useDispatch()
+// ////This is pre-Thunk code : 
+
+//   // useEffect(() => {
+//   //   fetch('http://localhost:3001/anecdotes')
+//   //     .then(res => res.json())
+//   //     .then(data => {
+//   //       dispatch(setAnecdotes(data))
+//   //     })
+//   // }, [dispatch])
+
+// useEffect(() => {
+//     dispatch(initializeAnecdotes())
+//   }, [dispatch])
+
+//   return (
+//     <div>
+//       <Notification />
+//       <Filter />
+//       <AnecdoteList />
+//       <NewAnecdote />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './request'
 import AnecdoteList from './components/AnecdoteList'
 import NewAnecdote from './components/NewAnecdotes'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
 const App = () => {
-  const dispatch = useDispatch()
-////This is pre-Thunk code : 
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false  
+  })
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/anecdotes')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       dispatch(setAnecdotes(data))
-  //     })
-  // }, [dispatch])
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
 
-useEffect(() => {
-    dispatch(initializeAnecdotes())
-  }, [dispatch])
+  if (result.isError) {
+    return (
+      <div>
+        <h2>Anecdote service not available</h2>
+        <p>Problems in server</p>
+      </div>
+    )
+  }
+
+  // ✅ 3️⃣ Success state
+  const anecdotes = result.data
 
   return (
     <div>
       <Notification />
       <Filter />
-      <AnecdoteList />
+      <AnecdoteList anecdotes={anecdotes} />
       <NewAnecdote />
     </div>
   )
--- part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -1,20 +1,57 @@
-import { useSelector, useDispatch } from 'react-redux'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { updateVote } from '../reducers/anecdoteReducer'
+// import { setNotificationTimed } from '../reducers/notificationReducer'
+// const AnecdoteList = () => {
+//   const dispatch = useDispatch()
+
+//   const anecdotes = useSelector(state => state.anecdotes)
+//   const filter = useSelector(state => state.filter)
+
+//   const visible = [...anecdotes]
+//     .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+//     .sort((a, b) => b.votes - a.votes)
+
+// const vote = (anecdote) => {
+//   dispatch(updateVote(anecdote))
+//   dispatch(setNotificationTimed(`you voted '${anecdote.content}'`, 5))
+// }
+
+//   return (
+//     <div>
+//       <h2>Anecdotes</h2>
+//       {visible.map(anecdote => (
+//         <div key={anecdote.id}>
+//           <div>{anecdote.content}</div>
+//           <div>
+//             has {anecdote.votes}
+//             <button onClick={() => vote(anecdote)}>vote</button>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default AnecdoteList
+
+
+
+import { useDispatch, useSelector } from 'react-redux'
 import { updateVote } from '../reducers/anecdoteReducer'
 import { setNotificationTimed } from '../reducers/notificationReducer'
-const AnecdoteList = () => {
-  const dispatch = useDispatch()
 
-  const anecdotes = useSelector(state => state.anecdotes)
+const AnecdoteList = ({ anecdotes }) => {
+  const dispatch = useDispatch()
   const filter = useSelector(state => state.filter)
 
   const visible = [...anecdotes]
     .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
     .sort((a, b) => b.votes - a.votes)
 
-const vote = (anecdote) => {
-  dispatch(updateVote(anecdote))
-  dispatch(setNotificationTimed(`you voted '${anecdote.content}'`, 5))
-}
+  const vote = (anecdote) => {
+    dispatch(updateVote(anecdote))
+    dispatch(setNotificationTimed(`you voted '${anecdote.content}'`, 5))
+  }
 
   return (
     <div>
